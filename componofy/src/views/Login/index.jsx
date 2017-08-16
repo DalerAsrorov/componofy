c55d7b0dd4b7d3d33b5ecb69aa9a638501746fd4
Daler Asrorov <asrorids@gmail.com>
2017-08-16 00:17:48 -0700

    More additions to styles

--- componofy/src/views/Login/index.jsx
+++ componofy/src/views/Login/index.jsx
@@ -2,12 +2,17 @@
 
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
+import Grid from 'material-ui/Grid';
 import Landing from 'components/Landing';
 import './login.css';
 
 class Login extends Component {
     render() {
-        return <Landing icon="someIcon" iconText="Authorize" title="Componofy" subTitle="by Daler Asrorov" />;
+        return (
+            <Grid className="login">
+                <Landing icon="someIcon" iconText="Authorize" title="Componofy" subTitle="by Daler Asrorov" />
+            </Grid>
+        );
     }
 }
 
