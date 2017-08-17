2976b47d14b3a93866a48ec759906088af9d2654
Daler Asrorov <asrorids@gmail.com>
2017-08-17 00:59:49 -0700

    Addes on Landing component UI

--- componofy/src/components/Landing/index.jsx
+++ componofy/src/components/Landing/index.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
 import Grid from 'material-ui/Grid';
 import Divider from 'material-ui/Divider';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import FaSpotify from 'react-icons/lib/fa/spotify';
 import './landing.css';
 
 class Landing extends Component {
@@ -14,25 +17,21 @@ class Landing extends Component {
 
     render() {
         return (
-            <Grid container spacing={0} className="landing" justify="center">
+            <Grid container spacing={8} className="landing" justify="center">
                 <Grid item xs={12}>
-                    <h1>
+                    <Typography type="root" color="accent" component="h1">
                         {this.props.title}
-                    </h1>
-                </Grid>
-                <Grid item xs={12}>
-                    <h5>
+                    </Typography>
+                    <Typography type="gutterBottom" color="secondary" component="p">
                         {this.props.subTitle}
-                    </h5>
-                    <Divider light />
+                    </Typography>
+                    <Divider default className="subhead-div-hr" />
                 </Grid>
                 <Grid item xs={12}>
-                    <h1>
-                        {this.props.icon}
-                    </h1>
-                    <h5>
+                    <Button raised color="primary">
+                        <FaSpotify />
                         {this.props.iconText}
-                    </h5>
+                    </Button>
                 </Grid>
             </Grid>
         );
