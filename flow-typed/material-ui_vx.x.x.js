d847ade5bf116aa637279fbf443912daba9af2e8
Daler Asrorov <asrorids@gmail.com>
2017-08-22 22:04:15 -0700

    Configured flow and installed flow-types

---
+++ flow-typed/material-ui_vx.x.x.js
@@ -0,0 +1,27 @@
+declare module 'material-ui' {
+    declare module.exports: any;
+}
+
+declare module 'material-ui/styles/MuiThemeProvider' {
+    declare module.exports: any;
+}
+
+declare module 'material-ui/Paper' {
+    declare module.exports: any;
+}
+
+declare module 'material-ui/Grid' {
+    declare module.exports: any;
+}
+
+declare module 'material-ui/styles/theme' {
+    declare module.exports: any;
+}
+
+declare module 'material-ui/styles' {
+    declare module.exports: any;
+}
+
+declare module 'components/Landing' {
+    declare module.exports: any;
+}
