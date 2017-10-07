7bc46add656e655e53d70d7a9833d5ab8860f7d8
Daler Asrorov <asrorids@gmail.com>
2017-10-06 23:53:20 -0700

    prototype future state to build UI first

--- src/components/MyPlaylists/index.jsx
+++ src/components/MyPlaylists/index.jsx
@@ -2,10 +2,23 @@ import React, { PureComponent } from 'react';
 import { getMyPlaylists } from '../../api';
 
 export default class MyPlaylists extends PureComponent {
+    state = {
+        // TODO: make this a global state in Redux
+        myPlaylists: [],
+        // Once the user reaches the last item,
+        // if there is more will make another request
+        // based on the offset (and limit)
+        isScrolledToTarget: false
+    };
+
     componentDidMount() {
         getMyPlaylists()
-            .then(playlists => {
-                console.log('Getting user playlist:', playlists);
+            .then(response => {
+                const { data: { body: { items: myPlaylists } } } = response;
+
+                this.setState({
+                    myPlaylists
+                });
             })
             .catch(error => {
                 console.log('Error:', error);
@@ -13,6 +26,10 @@ export default class MyPlaylists extends PureComponent {
     }
 
     render() {
+        const { myPlaylists } = this.state;
+
+        console.log(myPlaylists);
+
         return <h3>My playlists route </h3>;
     }
 }
