21003138bf777c216cece7af3750ef6596cabb38
Daler Asrorov <asrorids@gmail.com>
2017-10-29 23:02:53 -0700

    Utility function for anchor link creation

--- src/components/Track/Info.js
+++ src/components/Track/Info.js
@@ -4,6 +4,8 @@ import { withStyles } from 'material-ui/styles';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
 
+import './Track.css';
+
 const styles = theme => ({
     root: theme.mixins.gutters({
         paddingTop: 6,
@@ -15,37 +17,28 @@ const styles = theme => ({
     }
 });
 
+const createTypographyLink = (content, type, href) => (
+    <Typography
+        className="link-default"
+        type={type}
+        component="a"
+        href={href}
+        target="__blank"
+    >
+        {content}
+    </Typography>
+);
+
 const Info = props => {
     return (
         <div className={props.classes.root}>
-            <Typography
-                type="subheading"
-                component="a"
-                href={props.trackUrl}
-                target="__blank"
-                className={props.classes.link}
-            >
-                {props.trackName}
-            </Typography>
-            <Typography
-                type="body1"
-                component="a"
-                href={props.artistUrl}
-                target="__blank"
-                className={props.classes.link}
-            >
-                {props.artistName}
-            </Typography>
-            <Typography
-                type="caption"
-                component="a"
-                href={props.albumUrl}
-                target="__blank"
-                className={props.classes.link}
-                noWrap
-            >
-                {props.albumName}
-            </Typography>
+            {createTypographyLink(
+                props.trackName,
+                'subheading',
+                props.trackUrl
+            )}
+            {createTypographyLink(props.artistName, 'body1', props.artistUrl)}
+            {createTypographyLink(props.albumName, 'caption', props.albumUrl)}
         </div>
     );
 };
