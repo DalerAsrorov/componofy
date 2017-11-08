6bed4f291d3843dc2af130d58d2a2e6c0bbe58c6
Daler Asrorov <asrorids@gmail.com>
2017-11-07 21:59:24 -0800

    Added styling for the counter and text

--- src/components/FooterPanel/index.js
+++ src/components/FooterPanel/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'material-ui/Button';
 import { withStyles } from 'material-ui/styles';
+import { lightBlue } from 'material-ui/colors';
+import Typography from 'material-ui/Typography';
+import ArrowDropDownCircle from 'material-ui-icons/ArrowDropDownCircle';
+import Badge from 'material-ui/Badge';
 
 import './FooterPanel.css';
 
 const styles = theme => ({
+    badge: {
+        margin: `0 ${theme.spacing.unit * 2}px`
+    },
+
+    maintext: {},
+
     loadmore: {
         width: '100%'
     }
@@ -18,9 +28,15 @@ export const FooterPanel = props => (
         color="accent"
         className={props.classes.loadmore}
     >
-        {/* To do: Replace span with Material UI component */}
-        {props.mainText}
-        <span>{props.circleText}</span>
+        <Badge
+            className={props.classes.badge + ' footer-panel-badge'}
+            color="primary"
+            badgeContent={props.circleText ? props.circleText : ''}
+        >
+            <Typography type="subheading" className={props.classes.root}>
+                {props.mainText}
+            </Typography>
+        </Badge>
     </Button>
 );
 
