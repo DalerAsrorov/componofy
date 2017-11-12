9ce0647c7e8fa51bc9f36556c07bdf9fbf1d0d65
Daler Asrorov <asrorids@gmail.com>
2017-11-11 17:52:20 -0800

    Set up enzyme and configured Nav unit test

--- src/components/Nav/Nav.spec.js
+++ src/components/Nav/Nav.spec.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import Enzyme, { shallow, render, mount } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow, render, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
 import Nav from './';
 
-// React 16 Enzyme adapter
-Enzyme.configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() });
 
 // props used throughout test
 const navigateTo = jest.fn();
@@ -20,20 +19,20 @@ const classes = {
 it('Nav renders properly', () => {
     let nav = shallow(<Nav classes={classes} navigateTo={navigateTo} />);
 
-    expect(nav).toMatchSnapshot();
+    return expect(nav).toMatchSnapshot();
 });
 
 it('Nav navigateTo prop is not being called on render', () => {
     let nav = shallow(<Nav classes={classes} navigateTo={navigateTo} />);
 
-    expect(navigateTo.mock.calls.length).toBe(0);
+    return expect(navigateTo.mock.calls.length).toBe(0);
 });
 
 it('Nav has correct classes for styling', () => {
     let nav = shallow(<Nav classes={classes} navigateTo={navigateTo} />);
     let classNames = nav.prop('classes');
 
-    expect(classNames).toMatchObject({
+    return expect(classNames).toMatchObject({
         root: expect.any(String),
         tabContainer: expect.any(String)
     });
