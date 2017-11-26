e4b74d156624ba7cdbb8057a1636e830671811d4
Daler Asrorov <asrorids@gmail.com>
2017-11-25 20:11:10 -0800

    Playlist img (#68)
    Added playlist cover image to the Playlist component

--- src/components/Playlist/index.js
+++ src/components/Playlist/index.js
@@ -4,13 +4,16 @@ import Collapse from 'material-ui/transitions/Collapse';
 import { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import { withStyles } from 'material-ui/styles';
 import { PlaylistAdd, PlaylistAddCheck } from 'material-ui-icons';
-import { isEmpty } from 'ramda';
+import Avatar from 'material-ui/Avatar';
+import { isEmpty, head } from 'ramda';
 import { PLAYLIST_PROPTYPE, USER_PROPTYPE } from '../../utils/constants';
 import List from '../List';
 
 import './Playlist.css';
 
 const styles = theme => ({
+    playlistAvatar: {},
+
     nested: {
         paddingLeft: theme.spacing.unit * 4
     }
@@ -21,8 +24,8 @@ class Playlist extends PureComponent {
         containsThisPlaylist: PropTypes.bool.isRequired,
         playlist: PLAYLIST_PROPTYPE.isRequired,
         onClickIcon: PropTypes.func.isRequired,
-        user: USER_PROPTYPE.isRequired,
-        classes: PropTypes.object
+        classes: PropTypes.object.isRequired,
+        user: USER_PROPTYPE.isRequired
     };
 
     componentDidMount = () => {
@@ -54,8 +57,9 @@ class Playlist extends PureComponent {
     };
 
     render() {
-        const { playlist, containsThisPlaylist } = this.props;
-        const { tracks: { list: tracks } } = playlist;
+        const { playlist, containsThisPlaylist, classes } = this.props;
+        const { tracks: { list: tracks }, images: playlistImages } = playlist;
+        const playlistImage = head(playlistImages);
         let playlistIconComponent = containsThisPlaylist ? (
             <PlaylistAddCheck />
         ) : (
@@ -76,6 +80,12 @@ class Playlist extends PureComponent {
                     >
                         {playlistIconComponent}
                     </ListItemIcon>
+                    <Avatar
+                        alt={`${playlist.name} playlist cover`}
+                        src={playlistImage.url}
+                        className={classes.playlistAvatar}
+                    />
+
                     <ListItemText inset primary={playlist.name} />
                 </ListItem>
                 <Collapse
