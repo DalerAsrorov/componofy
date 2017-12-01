dfba72d42539c8a27dcbd3464db1ff92bc87d6cc
Daler Asrorov <asrorids@gmail.com>
2017-11-30 22:31:19 -0800

    created modal component and connected to componofy page

--- src/components/ComponofyPlaylists/index.js
+++ src/components/ComponofyPlaylists/index.js
@@ -7,6 +7,7 @@ import Badge from 'material-ui/Badge';
 import { MenuItem } from 'material-ui/Menu';
 import { Divider } from 'material-ui';
 import { withStyles } from 'material-ui/styles';
+import Dialog from '../Dialog';
 import { lightBlue } from 'material-ui/colors';
 import { PlaylistAddCheck, Audiotrack } from 'material-ui-icons';
 import { Search as SearchIcon } from 'material-ui-icons';
@@ -67,6 +68,7 @@ const styles = theme => ({
 class ComponofyPlaylists extends PureComponent {
     state = {
         shouldFilterList: false,
+        isOpenModal: false,
         settingsIsOpen: false,
         canScrollUp: false,
         anchorEl: null
@@ -146,8 +148,7 @@ class ComponofyPlaylists extends PureComponent {
     };
 
     _handleComponofy = () => {
-        console.log('invoked handle componfy');
-
+        this.setState({ isOpenModal: true });
         // full screen dialog
         // create state for the modal form with redux
     };
@@ -158,6 +159,10 @@ class ComponofyPlaylists extends PureComponent {
         scroll.scrollToTop({
             duration: SCROLL_DURATION
         });
+    };
+
+    _handleClickCloseModal = () => {
+        this.setState({ isOpenModal: false });
     };
 
     componentDidMount() {
@@ -187,6 +192,7 @@ class ComponofyPlaylists extends PureComponent {
         } = this.props;
         const {
             shouldFilterList,
+            isOpenModal,
             settingsIsOpen,
             canScrollUp,
             anchorEl
@@ -308,6 +314,11 @@ class ComponofyPlaylists extends PureComponent {
                         mainText={mainText}
                         style={footerStyle}
                         mainButtonStyle={mainButtonStyle}
+                    />
+                    <Dialog
+                        onClickClose={this._handleClickCloseModal}
+                        isOpen={isOpenModal}
+                        title="New playlist info"
                     />
                 </div>
             </HotKeys>
--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
-import Dialog from 'material-ui/Dialog';
+import MaterialDialog from 'material-ui/Dialog';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
@@ -10,7 +10,7 @@ import Typography from 'material-ui/Typography';
 import CloseIcon from 'material-ui-icons/Close';
 import Slide from 'material-ui/transitions/Slide';
 
-const styles = {
+const styles = theme => ({
     appBar: {
         position: 'relative'
     },
@@ -18,28 +18,32 @@ const styles = {
     flex: {
         flex: 1
     }
-};
+});
 
-const Transition = props => <Slide direction={props.direction} {...props} />;
+const Transition = props => <Slide direction="up" {...props} />;
 
 class Dialog extends PureComponent {
     static propTypes = {
-        isOpen: PropTypes.bool.isRequired
+        isOpen: PropTypes.bool.isRequired,
+        onClickClose: PropTypes.func.isRequired,
+        title: PropTypes.string.isRequired
     };
 
     render() {
+        const { title, isOpen, onClickClose, classes } = this.props;
+
         return (
-            <Dialog
-                {...this.props}
-                open={this.state.open}
-                onRequestClose={this.handleRequestClose}
+            <MaterialDialog
+                fullScreen
+                open={isOpen}
+                onRequestClose={onClickClose}
                 transition={Transition}
             >
                 <AppBar className={classes.appBar}>
                     <Toolbar>
                         <IconButton
                             color="contrast"
-                            onClick={this.handleRequestClose}
+                            onClick={onClickClose}
                             aria-label="Close"
                         >
                             <CloseIcon />
@@ -49,32 +53,14 @@ class Dialog extends PureComponent {
                             color="inherit"
                             className={classes.flex}
                         >
-                            Sound
+                            {title}
                         </Typography>
-                        <Button
-                            color="contrast"
-                            onClick={this.handleRequestClose}
-                        >
-                            save
-                        </Button>
                     </Toolbar>
                 </AppBar>
-                <List>
-                    <ListItem button>
-                        <ListItemText
-                            primary="Phone ringtone"
-                            secondary="Titania"
-                        />
-                    </ListItem>
-                    <Divider />
-                    <ListItem button>
-                        <ListItemText
-                            primary="Default notification ringtone"
-                            secondary="Tethys"
-                        />
-                    </ListItem>
-                </List>
-            </Dialog>
+                <div>container</div>
+            </MaterialDialog>
         );
     }
 }
+
+export default withStyles(styles)(Dialog);
