054e3872f4f7faf7388ac236da807f106aceae00
Daler Asrorov <asrorids@gmail.com>
2017-12-17 22:43:08 -0800

    Make auth more secure

--- server.js
+++ server.js
@@ -99,10 +99,6 @@ const startApp = async () => {
                 return h.redirect(authUrl);
             },
             config: {
-                cors: {
-                    credentials: true,
-                    origin: ['*']
-                },
                 description: 'Receives confirmation to start authentication.',
                 notes: 'Authentication process will redirect',
                 tags: ['api', 'auth', 'user']
@@ -134,10 +130,6 @@ const startApp = async () => {
                     .catch(error => console.error(error));
             },
             config: {
-                cors: {
-                    credentials: true,
-                    origin: ['*']
-                },
                 description:
                     'Given code from Spotify authentication server, generates token and `re`directs back to client app.',
                 notes: 'Accepts the code value',
