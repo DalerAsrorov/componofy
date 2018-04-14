709e8c328cf9c2a743a5165824227a5a5c39f5ce
Daler Asrorov <asrorids@gmail.com>
2018-04-14 02:11:51 -0700

    Use more functional syntax in actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -21,98 +21,76 @@ export const addErrorToApp = (
   message = '',
   timeout = 10000,
   errorId = Date.now()
-) => {
-  return {
-    type: ADD_ERROR_TO_APP,
-    errorId,
-    timeout,
-    message
-  };
-};
+) => ({
+  type: ADD_ERROR_TO_APP,
+  errorId,
+  timeout,
+  message
+});
 
 export const REQUEST_PLAYLISTS = 'REQUEST_PLAYLISTS';
-const requestPlaylists = () => {
-  return {
-    type: REQUEST_PLAYLISTS
-  };
-};
+const requestPlaylists = () => ({
+  type: REQUEST_PLAYLISTS
+});
 
 export const RECEIVE_PLAYLISTS = 'RECEIVE_PLAYLISTS';
-const receivePlaylists = json => {
-  return {
-    type: RECEIVE_PLAYLISTS,
-    playlists: json.data.body ? json.data.body.items : [],
-    numberOfTracks: json.data.body ? json.data.body.total : 0,
-    receivedAt: Date.now()
-  };
-};
+const receivePlaylists = json => ({
+  type: RECEIVE_PLAYLISTS,
+  playlists: json.data.body ? json.data.body.items : [],
+  numberOfTracks: json.data.body ? json.data.body.total : 0,
+  receivedAt: Date.now()
+});
 
-export const fetchMyPlaylists = offset => {
-  return dispatch => {
-    dispatch(requestPlaylists());
-    return getMyPlaylists(offset).then(json =>
-      dispatch(receivePlaylists(json))
-    );
-  };
+export const fetchMyPlaylists = offset => dispatch => {
+  dispatch(requestPlaylists());
+  return getMyPlaylists(offset).then(json => dispatch(receivePlaylists(json)));
 };
 
 export const RECEIVED_AUTH_STATE = 'RECEIVED_AUTH_STATE';
-const receivedAuthState = userInfo => {
-  return {
-    type: RECEIVED_AUTH_STATE,
-    userInfo
-  };
-};
+const receivedAuthState = userInfo => ({
+  type: RECEIVED_AUTH_STATE,
+  userInfo
+});
 
-export const checkIfAuthenticated = () => {
-  return dispatch => {
-    return getMyStatus().then(
-      userInfo => {
-        const { isAuthenticated } = userInfo;
-        dispatch(receivedAuthState(userInfo));
+export const checkIfAuthenticated = () => dispatch => {
+  return getMyStatus().then(
+    userInfo => {
+      const { isAuthenticated } = userInfo;
+      dispatch(receivedAuthState(userInfo));
 
-        if (!isAuthenticated) {
-          dispatch(push('/'));
-        }
-      },
-      error => addErrorToApp(`Failed to get the user's authentication status.`)
-    );
-  };
+      if (!isAuthenticated) {
+        dispatch(push('/'));
+      }
+    },
+    error => addErrorToApp(`Failed to get the user's authentication status.`)
+  );
 };
 
 export const SET_PLAYLIST_OPEN = 'SET_PLAYLIST_OPEN';
-export const setPlaylistOpen = (playlistID, isOpen) => {
-  return {
-    type: SET_PLAYLIST_OPEN,
-    playlistID,
-    isOpen
-  };
-};
+export const setPlaylistOpen = (playlistID, isOpen) => ({
+  type: SET_PLAYLIST_OPEN,
+  playlistID,
+  isOpen
+});
 
 export const SET_MY_PLAYLIST_VISITED = 'SET_MY_PLAYLIST_VISITED';
-export const setMyPlaylistVisited = isVisited => {
-  return {
-    type: SET_MY_PLAYLIST_VISITED,
-    isVisited
-  };
-};
+export const setMyPlaylistVisited = isVisited => ({
+  type: SET_MY_PLAYLIST_VISITED,
+  isVisited
+});
 
 export const REQUEST_PLAYLIST_TRACKS = 'REQUEST_PLAYLIST_TRACKS';
-export const requestPlaylistTracks = () => {
-  return {
-    type: REQUEST_PLAYLIST_TRACKS
-  };
-};
+export const requestPlaylistTracks = () => ({
+  type: REQUEST_PLAYLIST_TRACKS
+});
 
 export const RECEIVED_PLAYLIST_TRACKS = 'RECEIVED_PLAYLIST_TRACKS';
-export const receivedPlaylistTracks = (playlistID, tracks) => {
-  return {
-    type: RECEIVED_PLAYLIST_TRACKS,
-    receivedTracksAt: Date.now(),
-    tracks: formatTracks(tracks),
-    playlistID
-  };
-};
+export const receivedPlaylistTracks = (playlistID, tracks) => ({
+  type: RECEIVED_PLAYLIST_TRACKS,
+  receivedTracksAt: Date.now(),
+  tracks: formatTracks(tracks),
+  playlistID
+});
 
 export const RECEIVED_PUBLIC_PLAYLIST_TRACKS =
   'RECEIVED_PUBLIC_PLAYLIST_TRACKS';
@@ -123,88 +101,80 @@ export const receivedPublicPlaylistTracks = (playlistId, tracks) => ({
   playlistId
 });
 
-export const fetchPlaylistTracks = (userId, playlistId) => {
-  return (dispatch, getState) => {
+export const fetchPlaylistTracks = (userId, playlistId) => (
+  dispatch,
+  getState
+) => {
+  const {
+    router: {
+      location: { pathname }
+    },
+    navigation: { indexToRouteMap }
+  } = getState();
+
+  return getPlaylistTracks(userId, playlistId).then(response => {
     const {
-      router: {
-        location: { pathname }
-      },
-      navigation: { indexToRouteMap }
-    } = getState();
+      data: { body: payload }
+    } = response;
+    let tracks = [];
 
-    return getPlaylistTracks(userId, playlistId).then(response => {
-      const {
-        data: { body: payload }
-      } = response;
-      let tracks = [];
+    if (payload) {
+      tracks = payload.items;
+    }
 
-      if (payload) {
-        tracks = payload.items;
-      }
-
-      switch (pathname) {
-        case indexToRouteMap[0]:
-          dispatch(receivedPlaylistTracks(playlistId, tracks));
-          break;
-        case indexToRouteMap[1]:
-          dispatch(receivedPublicPlaylistTracks(playlistId, tracks));
-          break;
-        default:
-          break;
-      }
-    });
-  };
+    switch (pathname) {
+      case indexToRouteMap[0]:
+        dispatch(receivedPlaylistTracks(playlistId, tracks));
+        break;
+      case indexToRouteMap[1]:
+        dispatch(receivedPublicPlaylistTracks(playlistId, tracks));
+        break;
+      default:
+        break;
+    }
+  });
 };
 
 export const SET_MERGER_STATUS = 'SET_MERGER_STATUS';
-export const setMergerStatus = (status, statusText) => {
-  return {
-    type: SET_MERGER_STATUS,
-    statusText,
-    status
-  };
-};
+export const setMergerStatus = (status, statusText) => ({
+  type: SET_MERGER_STATUS,
+  statusText,
+  status
+});
 
 export const SET_FINAL_PLAYLIST_URL = 'SET_FINAL_PLAYLIST_URL';
-export const setFinalPlaylistUrl = url => {
-  return {
-    type: SET_FINAL_PLAYLIST_URL,
-    url
-  };
-};
+export const setFinalPlaylistUrl = url => ({
+  type: SET_FINAL_PLAYLIST_URL,
+  url
+});
 
 export const CLEAR_FINAL_DATA = 'CLEAR_FINAL_DATA';
-export const clearFinalData = () => {
-  return {
-    type: CLEAR_FINAL_DATA
-  };
-};
+export const clearFinalData = () => ({
+  type: CLEAR_FINAL_DATA
+});
 
 export const CLEAR_MY_DATA = 'CLEAR_MY_DATA';
-export const clearMyData = () => {
-  return {
-    type: CLEAR_MY_DATA
-  };
-};
+export const clearMyData = () => ({
+  type: CLEAR_MY_DATA
+});
 
 export const CLEAR_PUBLIC_DATA = 'CLEAR_PUBLIC_DATA';
-export const clearPublicData = () => {
-  return {
-    type: CLEAR_PUBLIC_DATA
-  };
-};
+export const clearPublicData = () => ({
+  type: CLEAR_PUBLIC_DATA
+});
 
 export const SET_OPEN_STATUS_MY_PLAYLISTS = 'SET_OPEN_STATUS_MY_PLAYLISTS';
-export const setOpenStatusMyPlaylists = isOpen => {
-  return {
-    type: SET_OPEN_STATUS_MY_PLAYLISTS,
-    isOpen
-  };
-};
+export const setOpenStatusMyPlaylists = isOpen => ({
+  type: SET_OPEN_STATUS_MY_PLAYLISTS,
+  isOpen
+});
 
-// May use this method in the future to
-// make routing share the same open status
-// but not needed for now
+/**
+ * May use this method in the future to control the
+ * routing of navigation to share statsuses between
+ * different playlist pages.
+ * @param {*} isOpen
+ */
 export const setOpenStatusForAllPlaylists = isOpen => (dispatch, getState) => {
   const {
     router: {
@@ -228,130 +198,102 @@ export const setOpenStatusForAllPlaylists = isOpen => (dispatch, getState) => {
 };
 
 export const ADD_PLAYLIST_TO_FINAL = 'ADD_PLAYLIST_TO_FINAL';
-export const addPlaylistToFinal = playlist => {
-  return {
-    type: ADD_PLAYLIST_TO_FINAL,
-    receivedAt: Date.now(),
-    playlist
-  };
-};
+export const addPlaylistToFinal = playlist => ({
+  type: ADD_PLAYLIST_TO_FINAL,
+  receivedAt: Date.now(),
+  playlist
+});
 
 export const REMOVE_PLAYLIST_FROM_FINAL = 'REMOVE_PLAYLIST_FROM_FINAL';
-export const removePlaylistFromFinal = playlist => {
-  return {
-    type: REMOVE_PLAYLIST_FROM_FINAL,
-    receivedAt: Date.now(),
-    playlist
-  };
-};
+export const removePlaylistFromFinal = playlist => ({
+  type: REMOVE_PLAYLIST_FROM_FINAL,
+  receivedAt: Date.now(),
+  playlist
+});
 
 export const ADD_PLAYLIST_TRACK_TO_FINAL = 'ADD_PLAYLIST_TRACK_TO_FINAL';
-export const addPlaylistTrackToFinal = (track, playlist) => {
-  return {
-    type: ADD_PLAYLIST_TRACK_TO_FINAL,
-    receivedAt: Date.now(),
-    playlist,
-    track
-  };
-};
+export const addPlaylistTrackToFinal = (track, playlist) => ({
+  type: ADD_PLAYLIST_TRACK_TO_FINAL,
+  receivedAt: Date.now(),
+  playlist,
+  track
+});
 
 export const REMOVE_PLAYLIST_TRACK_FROM_FINAL =
   'REMOVE_PLAYLIST_TRACK_FROM_FINAL';
-export const removePlaylistTrackFromFinal = (track, playlist) => {
-  return {
-    type: REMOVE_PLAYLIST_TRACK_FROM_FINAL,
-    receivedAt: Date.now(),
-    playlist,
-    track
-  };
-};
+export const removePlaylistTrackFromFinal = (track, playlist) => ({
+  type: REMOVE_PLAYLIST_TRACK_FROM_FINAL,
+  receivedAt: Date.now(),
+  playlist,
+  track
+});
 
 // Nav actions
 export const SET_NAV_INDEX = 'SET_NAV_INDEX';
-export const setNavIndex = (index = 0) => {
-  return {
-    type: SET_NAV_INDEX,
-    index
-  };
-};
+export const setNavIndex = (index = 0) => ({
+  type: SET_NAV_INDEX,
+  index
+});
 
 // My playlists search term
 export const SET_MY_SEARCH_TERM = 'SET_MY_SEARCH_TERM';
-export const setMySearchTerm = (searchTerm = '') => {
-  return {
-    type: SET_MY_SEARCH_TERM,
-    searchTerm
-  };
-};
+export const setMySearchTerm = (searchTerm = '') => ({
+  type: SET_MY_SEARCH_TERM,
+  searchTerm
+});
 
 // final playlist actions
 export const SET_FINAL_PLAYLIST_OPEN = 'SET_FINAL_PLAYLIST_OPEN';
-export const setFinalPlaylistOpen = (playlistID, isOpen) => {
-  return {
-    type: SET_FINAL_PLAYLIST_OPEN,
-    playlistID,
-    isOpen
-  };
-};
+export const setFinalPlaylistOpen = (playlistID, isOpen) => ({
+  type: SET_FINAL_PLAYLIST_OPEN,
+  playlistID,
+  isOpen
+});
 
 export const SET_FINAL_SEARCH_TERM = 'SET_FINAL_SEARCH_TERM';
-export const setFinalSearchTerm = (searchTerm = '') => {
-  return {
-    type: SET_FINAL_SEARCH_TERM,
-    searchTerm
-  };
-};
+export const setFinalSearchTerm = (searchTerm = '') => ({
+  type: SET_FINAL_SEARCH_TERM,
+  searchTerm
+});
 
 export const SET_OPEN_STATUS_FINAL_PLAYLISTS =
   'SET_OPEN_STATUS_FINAL_PLAYLISTS';
-export const setOpenStatusFinalPlaylists = (isOpen = false) => {
-  return {
-    type: SET_OPEN_STATUS_FINAL_PLAYLISTS,
-    isOpen
-  };
-};
+export const setOpenStatusFinalPlaylists = (isOpen = false) => ({
+  type: SET_OPEN_STATUS_FINAL_PLAYLISTS,
+  isOpen
+});
 
 export const SET_NEW_PLAYLIST_DESC = 'SET_NEW_PLAYLIST_DESC';
-export const setNewPlaylistDesc = playlistDesc => {
-  return {
-    type: SET_NEW_PLAYLIST_DESC,
-    playlistDesc
-  };
-};
+export const setNewPlaylistDesc = playlistDesc => ({
+  type: SET_NEW_PLAYLIST_DESC,
+  playlistDesc
+});
 
 export const SET_FINAL_PLAYLIST_PUBLIC = 'SET_FINAL_PLAYLIST_PUBLIC';
-export const setFinalPlaylistPublic = isPublic => {
-  return {
-    type: SET_FINAL_PLAYLIST_PUBLIC,
-    isPublic
-  };
-};
+export const setFinalPlaylistPublic = isPublic => ({
+  type: SET_FINAL_PLAYLIST_PUBLIC,
+  isPublic
+});
 
 export const SET_FINAL_PLAYLIST_IMAGE_URI = 'SET_FINAL_PLAYLIST_IMAGE_URI';
-export const setFinalPlaylistImageURI = imageUri => {
-  return {
-    type: SET_FINAL_PLAYLIST_IMAGE_URI,
-    imageUri
-  };
-};
+export const setFinalPlaylistImageURI = imageUri => ({
+  type: SET_FINAL_PLAYLIST_IMAGE_URI,
+  imageUri
+});
 
 export const REMOVE_ERROR_FROM_APP = 'REMOVE_ERROR_FROM_APP';
-export const removeErrorFromApp = errorId => {
-  return {
-    type: REMOVE_ERROR_FROM_APP,
-    errorId
-  };
-};
+export const removeErrorFromApp = errorId => ({
+  type: REMOVE_ERROR_FROM_APP,
+  errorId
+});
 
-export const finalizeProcessing = finalPlaylistUrl => {
-  return dispatch => {
-    dispatch(setMergerStatus(true, 'Finished!'));
-    dispatch(setMergerStatus(false));
-    dispatch(setFinalPlaylistUrl(finalPlaylistUrl));
-    dispatch(clearFinalData());
-    dispatch(clearPublicData());
-    dispatch(clearMyData());
-  };
+export const finalizeProcessing = finalPlaylistUrl => dispatch => {
+  dispatch(setMergerStatus(true, 'Finished!'));
+  dispatch(setMergerStatus(false));
+  dispatch(setFinalPlaylistUrl(finalPlaylistUrl));
+  dispatch(clearFinalData());
+  dispatch(clearPublicData());
+  dispatch(clearMyData());
 };
 
 export const launchPlaylistMerger = playlistName => (dispatch, getState) => {
@@ -417,111 +359,92 @@ export const launchPlaylistMerger = playlistName => (dispatch, getState) => {
 
 // PUBLIC PLAYLISTS ACTIONS
 export const SET_PUBLIC_SEARCH_TERM = 'SET_PUBLIC_SEARCH_TERM';
-export const setPublicSearchTerm = (searchTerm = '') => {
-  return {
-    type: SET_PUBLIC_SEARCH_TERM,
-    searchTerm
-  };
-};
+export const setPublicSearchTerm = (searchTerm = '') => ({
+  type: SET_PUBLIC_SEARCH_TERM,
+  searchTerm
+});
 
 export const SET_PUBLIC_PLAYLISTS_VISITED = 'SET_PUBLIC_PLAYLISTS_VISITED';
-export const setPublicPlaylistsVisited = (isVisited = true) => {
-  return {
-    type: SET_PUBLIC_PLAYLISTS_VISITED,
-    isVisited
-  };
-};
+export const setPublicPlaylistsVisited = (isVisited = true) => ({
+  type: SET_PUBLIC_PLAYLISTS_VISITED,
+  isVisited
+});
 
 export const REQUEST_SEARCHED_PLAYLISTS = 'REQUEST_SEARCHED_PLAYLISTS';
-export const requestSearchedPlaylists = () => {
-  return {
-    type: REQUEST_SEARCHED_PLAYLISTS
-  };
-};
+export const requestSearchedPlaylists = () => ({
+  type: REQUEST_SEARCHED_PLAYLISTS
+});
 
 export const RECEIVED_SEARCHED_PLAYLISTS = 'RECEIVED_SEARCHED_PLAYLISTS';
-export const receivedSearchedPlaylists = json => {
-  return {
-    type: RECEIVED_SEARCHED_PLAYLISTS,
-    playlists: json.data.body ? json.data.body.playlists.items : {},
-    numberOfTracks: json.data.body ? json.data.body.playlists.total : 0,
-    receivedAt: Date.now()
-  };
-};
+export const receivedSearchedPlaylists = json => ({
+  type: RECEIVED_SEARCHED_PLAYLISTS,
+  playlists: json.data.body ? json.data.body.playlists.items : {},
+  numberOfTracks: json.data.body ? json.data.body.playlists.total : 0,
+  receivedAt: Date.now()
+});
 
 export const SET_SEARCH_RESULTS_MESSAGE = 'SET_SEARCH_RESULTS_MESSAGE';
-export const setSearchResultsMessage = (message = '') => {
-  return {
-    type: SET_SEARCH_RESULTS_MESSAGE,
-    message
-  };
-};
+export const setSearchResultsMessage = (message = '') => ({
+  type: SET_SEARCH_RESULTS_MESSAGE,
+  message
+});
 
 export const CLEAN_PUBLIC_SEARCH_RESULTS = 'CLEAN_PUBLIC_SEARCH_RESULTS';
-export const cleanPublicSearchResults = () => {
-  return {
-    type: CLEAN_PUBLIC_SEARCH_RESULTS
-  };
-};
+export const cleanPublicSearchResults = () => ({
+  type: CLEAN_PUBLIC_SEARCH_RESULTS
+});
 
-export const searchPublicPlaylists = (shouldLoadMore = false) => {
-  return (dispatch, getState) => {
-    const {
-      publicPlaylists: { searchTerm, currentOffset }
-    } = getState();
+export const searchPublicPlaylists = (shouldLoadMore = false) => (
+  dispatch,
+  getState
+) => {
+  const {
+    publicPlaylists: { searchTerm, currentOffset }
+  } = getState();
 
-    dispatch(requestSearchedPlaylists());
+  dispatch(requestSearchedPlaylists());
 
-    if (shouldLoadMore) {
-      return searchPlaylists(searchTerm, currentOffset).then(json => {
-        dispatch(receivedSearchedPlaylists(json));
-      });
-    }
-
-    dispatch(cleanPublicSearchResults());
-    // start from 0 offset if the search request is different
-    return searchPlaylists(searchTerm, 0).then(json => {
+  if (shouldLoadMore) {
+    return searchPlaylists(searchTerm, currentOffset).then(json => {
       dispatch(receivedSearchedPlaylists(json));
-      dispatch(setSearchResultsMessage(`Results for "${searchTerm}"`));
     });
-  };
+  }
+
+  dispatch(cleanPublicSearchResults());
+  // start from 0 offset if the search request is different
+  return searchPlaylists(searchTerm, 0).then(json => {
+    dispatch(receivedSearchedPlaylists(json));
+    dispatch(setSearchResultsMessage(`Results for "${searchTerm}"`));
+  });
 };
 
 export const SET_PUBLIC_PLAYLIST_OPEN = 'SET_PUBLIC_PLAYLIST_OPEN';
-export const setPublicPlaylistOpen = (playlistId, isOpen) => {
-  return {
-    type: SET_PUBLIC_PLAYLIST_OPEN,
-    playlistId,
-    isOpen
-  };
-};
+export const setPublicPlaylistOpen = (playlistId, isOpen) => ({
+  type: SET_PUBLIC_PLAYLIST_OPEN,
+  playlistId,
+  isOpen
+});
 
 export const SET_OPEN_STATUS_PUBLIC_PLAYLISTS =
   'SET_OPEN_STATUS_PUBLIC_PLAYLISTS';
-export const setOpenStatusPublicPlaylists = isOpen => {
-  return {
-    type: SET_OPEN_STATUS_PUBLIC_PLAYLISTS,
-    isOpen
-  };
-};
+export const setOpenStatusPublicPlaylists = isOpen => ({
+  type: SET_OPEN_STATUS_PUBLIC_PLAYLISTS,
+  isOpen
+});
 
-export const logOutUser = () => {
-  return dispatch => {
-    return getLogOutUser().then(({ isAuthenticated }) => {
-      if (!isAuthenticated) {
-        dispatch(push('/'));
-      }
-    });
-  };
+export const logOutUser = () => dispatch => {
+  return getLogOutUser().then(({ isAuthenticated }) => {
+    if (!isAuthenticated) {
+      dispatch(push('/'));
+    }
+  });
 };
 
 export const SET_COMPONOFY_MODE = 'SET_COMPONOFY_MODE';
-export const setComponofyMode = hasChosenNewCreate => {
-  return {
-    type: SET_COMPONOFY_MODE,
-    hasChosenNewCreate
-  };
-};
+export const setComponofyMode = hasChosenNewCreate => ({
+  type: SET_COMPONOFY_MODE,
+  hasChosenNewCreate
+});
 
 export const REQUEST_MY_PLAYLISTS_FOR_SELECTION =
   'REQUEST_MY_PLAYLISTS_FOR_SELECTION';
@@ -531,14 +454,12 @@ export const requestMyPlaylistsForSelection = () => ({
 
 export const RECEIVE_MY_PLAYLISTS_FOR_SELECTION =
   'RECEIVE_MY_PLAYLISTS_FOR_SELECTION';
-const receiveMyPlaylistsForSelection = json => {
-  return {
-    type: RECEIVE_MY_PLAYLISTS_FOR_SELECTION,
-    playlists: json.data.body ? json.data.body.items : [],
-    totalNumberOfPlaylists: json.data.body ? json.data.body.total : 0,
-    receivedAt: Date.now()
-  };
-};
+const receiveMyPlaylistsForSelection = json => ({
+  type: RECEIVE_MY_PLAYLISTS_FOR_SELECTION,
+  playlists: json.data.body ? json.data.body.items : [],
+  totalNumberOfPlaylists: json.data.body ? json.data.body.total : 0,
+  receivedAt: Date.now()
+});
 
 export const fetchMyPlaylistsForSelection = (offset, limit) => dispatch => {
   dispatch(requestMyPlaylistsForSelection());
