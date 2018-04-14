4ac96da261c604497f65b04c9cd94176263fe56f
Daler Asrorov <asrorids@gmail.com>
2018-04-13 21:08:44 -0700

    Upgraded enzyme dependencies:

--- src/components/Nav/Nav.spec.js
+++ src/components/Nav/Nav.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Enzyme from 'enzyme';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Nav from './';
 
@@ -19,19 +19,19 @@ const classes = {
 
 describe('When Nav component is initialized', () => {
     it('Nav renders properly', () => {
-        let nav = shallow(<Nav classes={classes} navigateTo={navigateTo} />);
+        let nav = mount(<Nav classes={classes} navigateTo={navigateTo} />);
 
         return expect(nav).toMatchSnapshot();
     });
 
     it('Nav navigateTo prop is not being called on render', () => {
-        let nav = shallow(<Nav classes={classes} navigateTo={navigateTo} />);
+        let nav = mount(<Nav classes={classes} navigateTo={navigateTo} />);
 
         return expect(navigateTo.mock.calls.length).toBe(0);
     });
 
     it('Nav has correct classes for styling', () => {
-        let nav = shallow(<Nav classes={classes} navigateTo={navigateTo} />);
+        let nav = mount(<Nav classes={classes} navigateTo={navigateTo} />);
         let classNames = nav.prop('classes');
 
         return expect(classNames).toMatchObject({
