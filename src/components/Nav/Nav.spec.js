37fb7b81576ce8e97f7cc6cc98e3145564d91ac1
Daler Asrorov <asrorids@gmail.com>
2018-04-14 02:13:54 -0700

    Use more functional syntax in actions (#173)
    * format test
    
    * Use more functional syntax in actions

--- src/components/Nav/Nav.spec.js
+++ src/components/Nav/Nav.spec.js
@@ -8,45 +8,45 @@ Enzyme.configure({ adapter: new Adapter() });
 
 // props used throughout test
 const classes = {
-    root: '',
-    tabContainer: ''
+  root: '',
+  tabContainer: ''
 };
 
 const requiredProps = {
-    numberOfFinalPlaylists: 30,
-    location: {
-        pathname: 0
-    },
-    navigation: {
-        0: 'route1',
-        1: 'route2',
-        routeToIndexMap: jest.fn(),
-        index: 0
-    },
-    setNavIndex: jest.fn(),
-    navigateTo: jest.fn(),
-    classes
+  numberOfFinalPlaylists: 30,
+  location: {
+    pathname: 0
+  },
+  navigation: {
+    0: 'route1',
+    1: 'route2',
+    routeToIndexMap: jest.fn(),
+    index: 0
+  },
+  setNavIndex: jest.fn(),
+  navigateTo: jest.fn(),
+  classes
 };
 
 describe('When Nav component is initialized', () => {
-    it('Nav renders properly', () => {
-        let nav = shallow(<Nav {...requiredProps} />);
+  it('Nav renders properly', () => {
+    let nav = shallow(<Nav {...requiredProps} />);
 
-        expect(nav).toMatchSnapshot();
+    expect(nav).toMatchSnapshot();
+  });
+
+  it('Nav navigateTo prop is not being called on render', () => {
+    let nav = shallow(<Nav {...requiredProps} />);
+
+    expect(requiredProps.navigateTo.mock.calls.length).toBe(0);
+  });
+
+  it('Nav has correct classes for styling', () => {
+    let nav = shallow(<Nav {...requiredProps} />);
+    let classNames = nav.prop('classes');
+
+    Object.keys(classes).forEach(stylAttr => {
+      expect(classNames).toHaveProperty(stylAttr);
     });
-
-    it('Nav navigateTo prop is not being called on render', () => {
-        let nav = shallow(<Nav {...requiredProps} />);
-
-        expect(requiredProps.navigateTo.mock.calls.length).toBe(0);
-    });
-
-    it('Nav has correct classes for styling', () => {
-        let nav = shallow(<Nav {...requiredProps} />);
-        let classNames = nav.prop('classes');
-
-        Object.keys(classes).forEach(stylAttr => {
-            expect(classNames).toHaveProperty(stylAttr);
-        });
-    });
+  });
 });
